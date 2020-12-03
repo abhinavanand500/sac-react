4c54c1ef4dc3574f05c4faa8dbc0d1abba12c662
Abhinav Anand <abhinav.anand500@gmail.com>
2020-12-03 23:43:59 +0530

    Done with header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
-import Navbar from "./features/components/Navbar";
 import Home from "./features/home/Home";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
